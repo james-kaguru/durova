56cd6092d6e8196cea5a4548cde1e270da8abde6
James Kaguru <jimmykaguru@gmail.com>
2026-04-01 11:18:36 +0300

    switched to app folder

---
+++ app/(app)/page.tsx
@@ -0,0 +1,21 @@
+import { About } from "../about/about";
+import { Hero } from "./hero";
+import { Services } from "./services";
+import Faq from "./faq";
+import { Contact } from "./contact";
+import { Footer } from "./footer";
+import { Navbar } from "./navbar";
+
+export default function Home() {
+  return (
+    <div className="">
+      <Navbar />
+      <Hero />
+      <About />
+      <Services />
+      <Faq />
+      <Contact />
+      <Footer />
+    </div>
+  );
+}
