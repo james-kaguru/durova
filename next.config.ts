5a9d9f5d5736ff991195a4e0dafe41ddb12766fe
James Kaguru <jimmykaguru@gmail.com>
2026-04-01 11:31:19 +0300

    added payload plugin

--- next.config.ts
+++ next.config.ts
@@ -1,24 +1,25 @@
 import type { NextConfig } from "next";
+import { withPayload } from "@payloadcms/next/withPayload";
 
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'dev-shrift.s3.eu-central-1.amazonaws.com',
-        port: '',
-        pathname: '/**',
+        protocol: "https",
+        hostname: "dev-shrift.s3.eu-central-1.amazonaws.com",
+        port: "",
+        pathname: "/**",
       },
       {
-        protocol: 'https',
-        hostname: 'durova.s3.eu-central-1.amazonaws.com',
-        port: '',
-        pathname: '/**',
+        protocol: "https",
+        hostname: "durova.s3.eu-central-1.amazonaws.com",
+        port: "",
+        pathname: "/**",
       },
     ],
   },
-  allowedDevOrigins: ['192.168.100.5'],
+  allowedDevOrigins: ["192.168.100.5"],
 };
 
-export default nextConfig;
+export default withPayload(nextConfig);
